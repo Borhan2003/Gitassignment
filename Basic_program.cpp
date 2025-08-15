0421286ea2a12186b1e26c11a99acdbcede4304e
Borhan2003 <Borhan.u.2003@gmail.com>
2025-08-16 00:32:25 +0600

    factorial problem added

--- Basic_program.cpp
+++ Basic_program.cpp
@@ -6,9 +6,10 @@ int main()
     cout << "Enter a number: ";
     cin >> n;
     long long factorial = 1;
-    for (int i = 1; i <= n; i++)
+    for (int i = 5; i <= n; i++)
     {
         factorial *= i;
     }
     cout << "Factorial of " << n ;
+    cout<<
 }
